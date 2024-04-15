ba21d4adb25d33fea3009ebd5d802872b2895187
Mériadeck AMOUSSOU <mrdeck30@gmail.com>
2024-04-15 03:49:49 +0100

    update

--- campagne/apps.py
+++ campagne/apps.py
@@ -7,7 +7,7 @@ class CampagneConfig(AppConfig):
     name = "campagne"
     
     
-    api_key = "sk-AWPhrgrzegDAyMrgergbvTvrgergf04itkfpT3BlbkFJhRrt4ojmrwx3BmbCmBrnfvfregrgrgegr"
+    api_key = "sk-KSMy1h0RFefzGCS9zefeSU25SpaT3efazeBlbkFJ8HWzVmWZwguefzffX2U9NbMCO"
     # api_key=""
 
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo",
